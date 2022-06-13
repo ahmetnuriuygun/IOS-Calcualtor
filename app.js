32833f0a2ddd8308dbafaa33c5065eb1b27a74ef
ahmetnuygun <ahmetn.uygun@gmail.com>
2022-06-13 21:34:30 +0200

    the project was added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const number6 = document.querySelector(".number-6")
 const number7 = document.querySelector(".number-7")
 const number8 = document.querySelector(".number-8")
 const number9 = document.querySelector(".number-9")
-
+const numberButtons = document.querySelectorAll(".num")
 //* I assign to number buttons to the variables
 
 const plus = document.querySelector(".addition")
@@ -16,55 +16,43 @@ const subtraction = document.querySelector(".subtraction")
 const multiply = document.querySelector(".multiplication")
 const division = document.querySelector(".division")
 const percent = document.querySelector(".percent")
-const equal = document.querySelectorAll(".equal")
-
 //* I assign to operation buttons to the variables
+
+
+const equal = document.querySelectorAll(".equal")
+//* I assign to equal buttons to display the result
+
 
 const previous = document.querySelector(".previous-display")
 const current = document.querySelector(".current-display")
 
 //* I assign to display classes to the variables
 
-
-number0.onclick=()=>current.textContent = 0;
-number1.onclick=()=>current.textContent = 1;
-number2.onclick=()=>{current.textContent = 2;
-    const firstNumber= current.textContent;
-    console.log(firstNumber); 
-};
-number3.onclick=()=>current.textContent = 3;
-number4.onclick=()=>current.textContent = 4;
-number5.onclick=()=>current.textContent = 5;
-number6.onclick=()=>current.textContent = 6;
-number7.onclick=()=>current.textContent = 7;
-number8.onclick=()=>current.textContent = 8;
-number9.onclick=()=>current.textContent = 9;
+let UstYazilacakSayiJS = "";
+let AltYazilacakSayiJS = "";
+numberButtons.forEach((number) => {
+    number.addEventListener("click", () => {
+        AltYazilacakSayiJS += number.textContent;
+        //!ekrana bastır
+        current.textContent = AltYazilacakSayiJS;})})
 
 
-console.log(firstNumber);
 
-let result = null;  
 
 plus.onclick=()=>{
     previous.innerHTML=`${current.innerText} + `
-    current.textContent = " "
-    result =+ firstNumber}   
+    current.textContent = ""}   
 subtraction.onclick=()=>{
     previous.innerHTML=`${current.innerText} - `
-    current.textContent = " "}
+    current.textContent = ""}
 multiply.onclick=()=>{
     previous.innerHTML=`${current.innerText} * `
-    current.textContent = " "}
+    current.textContent = ""}
 division.onclick=()=>{
     previous.innerHTML=`${current.innerText} / `
-    current.textContent = " "}
+    current.textContent = ""}
 percent.onclick=()=>{
     previous.innerHTML=`${current.innerText} % `
-    current.textContent = " "}
-
-    console.log(result)
+    current.textContent = ""}
 
 
-equal.onclick=()=>{
-    current.textContent = `${result}
-}
