a9b111d77913d57ec20e9be5813fb653a2b99e8f
ahmetnuygun <ahmetn.uygun@gmail.com>
2022-06-15 16:06:33 +0200

    The project was added

--- app.js
+++ app.js
@@ -1,58 +1,146 @@
-const number0 = document.querySelector(".number-0")
-const number1 = document.querySelector(".number-1")
-const number2 = document.querySelector(".number-2")
-const number3 = document.querySelector(".number-3")
-const number4 = document.querySelector(".number-4")
-const number5 = document.querySelector(".number-5")
-const number6 = document.querySelector(".number-6")
-const number7 = document.querySelector(".number-7")
-const number8 = document.querySelector(".number-8")
-const number9 = document.querySelector(".number-9")
+
 const numberButtons = document.querySelectorAll(".num")
-//* I assign to number buttons to the variables
-
-const plus = document.querySelector(".addition")
-const subtraction = document.querySelector(".subtraction")
-const multiply = document.querySelector(".multiplication")
-const division = document.querySelector(".division")
-const percent = document.querySelector(".percent")
-//* I assign to operation buttons to the variables
+//* I assign to number button to the variables
 
 
-const equal = document.querySelectorAll(".equal")
-//* I assign to equal buttons to display the result
+const operationButtons = document.querySelectorAll(".operator")
+//* I assign to operation button to the variables
 
 
-const previous = document.querySelector(".previous-display")
-const current = document.querySelector(".current-display")
+const equal = document.querySelector(".equal")
+//* I assign to equal button to display the result
+
+
+const UstEkran = document.querySelector(".previous-display")
+const AltEkran = document.querySelector(".current-display")
 
 //* I assign to display classes to the variables
 
-let UstYazilacakSayiJS = "";
-let AltYazilacakSayiJS = "";
-numberButtons.forEach((number) => {
-    number.addEventListener("click", () => {
-        AltYazilacakSayiJS += number.textContent;
-        //!ekrana bastır
-        current.textContent = AltYazilacakSayiJS;})})
+let AltYazilacakSayiJS = []
+let UstYazilacakSayiJS = []
+let islem = ""
+let sonuc ;
+
+numberButtons.forEach((number)=>{
+    number.onclick=()=>{
+        AltYazilacakSayiJS.push(number.textContent)
+        AltEkran.textContent = AltYazilacakSayiJS.join("");
+        console.log(AltYazilacakSayiJS);
+    }
+})
+operationButtons.forEach((op)=>{
+    op.onclick=()=>{
+        UstYazilacakSayiJS = AltYazilacakSayiJS ;
+        AltYazilacakSayiJS = [];
+        islem = op.textContent
+        UstEkran.textContent = UstYazilacakSayiJS.join("") + islem;
+    }
+})
+equal.onclick=()=>{
+    switch(islem){
+        case "+" : 
+        sonuc = Number(UstYazilacakSayiJS.join(""))+ Number(AltYazilacakSayiJS.join(""));
+        AltEkran.textContent = sonuc
+        console.log(sonuc)
+        
+    }
+}
+
+const pm = document.querySelector(".pm")
+
+
+pm.onclick=()=>{
+    let yeniSayi = Number(AltYazilacakSayiJS.join("")); 
+    yeniSayi = -1 * yeniSayi;
+    AltYazilacakSayiJS = []
+    AltYazilacakSayiJS.push(yeniSayi)
+    console.log(AltYazilacakSayiJS)
+    AltEkran.textContent = AltYazilacakSayiJS
+}
+
+sifirlama = document.querySelector(".ac")
+sifirlama.onclick=()=>{
+    UstYazilacakSayiJS = []
+    AltYazilacakSayiJS = []
+    UstEkran.textContent = UstYazilacakSayiJS 
+    AltEkran.textContent = AltYazilacakSayiJS 
+}
 
 
 
 
-plus.onclick=()=>{
-    previous.innerHTML=`${current.innerText} + `
-    current.textContent = ""}   
-subtraction.onclick=()=>{
-    previous.innerHTML=`${current.innerText} - `
-    current.textContent = ""}
-multiply.onclick=()=>{
-    previous.innerHTML=`${current.innerText} * `
-    current.textContent = ""}
-division.onclick=()=>{
-    previous.innerHTML=`${current.innerText} / `
-    current.textContent = ""}
-percent.onclick=()=>{
-    previous.innerHTML=`${current.innerText} % `
-    current.textContent = ""}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// numberButtons.forEach((number)=>{
+//     number.addEventListener("click",e=>{
+//     AltYazilacakSayiJS += number.textContent 
+//     current.textContent = AltYazilacakSayiJS;
+    
+// operationButtons.forEach((op)=> {
+// op.addEventListener("click",e=>{
+//     islem = op.textContent;
+//     UstYazilacakSayiJS = AltYazilacakSayiJS;
+//     AltYazilacakSayiJS = ""
+//         previous.innerHTML = UstYazilacakSayiJS + islem
+    // });
+    // });
+    
+
+
+// if(islem != null){
+//     previous.textContent = `${UstYazilacakSayiJS} ${islem}`;
+// }if(islem == "+"){
+//     sonuc = UstYazilacakSayiJS + AltYazilacakSayiJS
+//     current.textContent = sonuc
+// }else if(islem == "-"){
+//     sonuc = UstYazilacakSayiJS - AltYazilacakSayiJS
+//     current.textContent = sonuc
+// }else if(islem == "x"){
+//     sonuc = UstYazilacakSayiJS * AltYazilacakSayiJS
+//     current.textContent = sonuc
+// }else if(islem == "÷"){
+//     sonuc = UstYazilacakSayiJS / AltYazilacakSayiJS
+//     current.textContent = sonuc
+// }else if(islem == "%"){
+//     sonuc = UstYazilacakSayiJS % AltYazilacakSayiJS
+//     current.textContent = sonuc
+// } 
+
+
+
+
+
+
+
+// });
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
